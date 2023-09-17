f206b23be54bef854fa9ed6f6f38f7a976e9ad34
Angus Wallbrink <anguswallbrink@Anguss-MacBook-Air-3.local>
2023-09-17 18:44:33 +1000

    Initial commit

---
+++ firmware/SoilMoistureSensor/SoilMoistureSensor.ino
@@ -0,0 +1,130 @@
+/*
+*  Weather Station App with Soil Moisture
+*  CGS Semester 2
+*  Task 2
+*  Author: Angus Wallbrink
+*/
+
+#include <math.h>
+#include <WiFi.h>
+#include <aREST.h>
+
+// Soil moisture sensor pin
+#define SOIL_MOISTURE_PIN 26 // Change this to the pin your sensor is connected to
+#define THRESHOLD 2000
+
+// Create aREST instance
+aREST rest = aREST();
+
+// WiFi parameters
+const char* ssid = "Proxima";
+const char* password = "centauri";
+
+//Static IP address configuration
+#define LISTEN_PORT 80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Variables to be exposed to the API
+int soilMoisture; // create a variable for soil moisture
+char* location = "Al Fresco";
+int timer = 72000;
+
+// Declare functions to be exposed to the API
+int ledControl(String command);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  // Init variables and expose them to REST API
+  rest.variable("soilMoisture", &soilMoisture); // Replace temperature and humidity
+  rest.variable("location", &location);
+
+  // Give name and ID to device
+  rest.set_id("112");
+  rest.set_name("alpha-112");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  IPAddress ip(192, 168, 1, 112); // Set static IP
+  IPAddress gateway(192, 168, 1, 1); // Set gateway
+  Serial.print(F("Setting static IP to : "));
+  Serial.println(ip);
+  IPAddress subnet(255, 255, 255, 0); // Set subnet
+  WiFi.config(ip, gateway, subnet);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop()
+{
+  // Reading soil moisture
+  soilMoisture = analogRead(SOIL_MOISTURE_PIN);
+
+  // Check if soil is dry or wet and print soil moisture value
+  if (soilMoisture < THRESHOLD) {
+    Serial.println("The soil is DRY");
+    Serial.print("Soil Moisture: ");
+    Serial.println(soilMoisture);
+  } else {
+    Serial.println("The soil is WET");
+    Serial.print("Soil Moisture: ");
+    Serial.println(soilMoisture);
+  }
+
+  // Check running time and reset if expired
+  if (timer <= 0)
+  {
+    delay(3000);
+    Serial.println("Resetting..");
+    ESP.restart();
+  }
+  else
+  {
+    Serial.print("Timer: ");
+    Serial.println(timer);
+    delay(5000);
+    timer--;
+  }
+
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+  while (!client.available())
+  {
+    delay(1);
+  }
+  rest.handle(client);
+}
+
+
+// Custom function accessible by the API
+int ledControl(String command)
+{
+
+  // Get state from command
+  int state = command.toInt();
+
+  digitalWrite(6, state);
+  return 1;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-const express= require('express')
+const express = require('express')
 const router = express.Router()
 const {fetch} = require('undici')
 
 
 router.get('/', async(req, res) => {
     try {
-        const data = await getApiData()
-        res.render('index', {obj: data} )
+        const data = await getApiData();
+        console.log(data);
+        res.render('index', {obj: data});
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('An error occurred while fetching data.');
@@ -15,7 +16,7 @@ router.get('/', async(req, res) => {
 
 async function getApiData() {
     try { 
-        const res = await fetch('http://192.168.1.xxx')
+        const res = await fetch('http://192.168.1.112')
 
         if (!res.ok) {
             throw new Error('Failed to fetch data')
@@ -28,8 +29,7 @@ async function getApiData() {
             location : txt.variables.location,
             id: txt.id,
             hardware: txt.hardware,
-            temperature : txt.variables.temperature,
-            humidity : txt.variables.humidity,
+            soilMoisture : txt.variables.soilMoisture,
         }
 
         console.log(txt)
